bdb8510df94e5bb2a05b8ddb40f942e189af5feb
BR4Y4NEXE <brayan11647.bvz@hotmail.com>
2025-06-09 00:07:35 -0600

    Correcciones en el backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,10 +33,13 @@ export default function Home() {
   const [historialDht, setHistorialDht] = useState<HistorialEntry[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const baseUrl = "https://api-pet-care-rc52.vercel.app";
+
+
   // Función para obtener el historial de DHT desde la API
   const fetchDhtHistory = async () => {
     try {
-      const response = await fetch('https://api-pet-care-rc52.vercel.app/api/get-dht-history');
+      const response = await fetch('{baseUrl}/api/get-dht-history');
       const data = await response.json();
       return data;
     } catch (error) {
@@ -48,7 +51,7 @@ export default function Home() {
   // Función para obtener los datos más recientes del DHT
   const fetchDhtCurrent = async () => {
     try {
-      const response = await fetch('https://api-pet-care-rc52.vercel.app/api/get-dht');
+      const response = await fetch('{baseUrl}/api/get-dht');
       const data = await response.json();
       return data;
     } catch (error) {
@@ -61,9 +64,9 @@ export default function Home() {
     const fetchData = async () => {
       try {
         const [infraredRes, servoRes, servoLogRes] = await Promise.all([
-          fetch("/api/get-infrared"),
-          fetch("/api/servo"),
-          fetch("/api/get-servo-log")
+          fetch("{baseUrl}/api/get-infrared"),
+          fetch("{baseUrl}/api/servo"),
+          fetch("{baseUrl}/api/get-servo-log")
         ]);
 
         const [infraredData, servoData, servoLogData] = await Promise.all([
@@ -98,7 +101,7 @@ export default function Home() {
 
   const toggleServo = async () => {
     try {
-      const res = await fetch("https://api-pet-care-rc52.vercel.app/api/servo", { method: "POST" });
+      const res = await fetch("{baseUrl}/api/servo", { method: "POST" });
       const data = await res.json();
       setServoStatus(data.status);
     } catch (error) {
